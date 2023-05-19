eac9351e5ce095416e353060796123a62341461f
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-05-19 06:49:12 -0500

    fixed dependency error

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const app = require("./app");
-const { PORT } = require("./config");
+const app = require("./app.js");
+const { PORT } = require("./config.js");
 
 app.listen(PORT, function () {
   console.log(`Started on http://localhost:${PORT}`);
